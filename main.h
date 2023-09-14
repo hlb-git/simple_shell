b8395d1784f6f0bf9b49a7daf673e730c66c41be
Love <mzeelovegeneral@gmail.com>
2023-09-14 10:52:59 +0100

    handles PATH

--- main.h
+++ main.h
@@ -7,7 +7,12 @@
 #include <unistd.h>
 #include <sys/types.h>
 #include <sys/wait.h>
+#include <sys/stat.h>
 
+/* Global enviroment variable */
+extern char **environ;
+
+char *find_path(char *cmd);
 void pretty(void);
 void execute_command(char *args[]);
 void handle_user_input(void);
